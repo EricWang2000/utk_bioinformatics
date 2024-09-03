6cb2db56a333d60ba4521d6286f78f9abd4fafbd
MungShu <mungshushen@gmail.com>
2024-09-03 09:46:38 -0400

    backend

---
+++ backend/django/djangodb/web/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path, re_path
+from . import views
+urlpatterns = [
+    # path("admin/")
+    path("", views.home, name="home"),
+    path("add/", views.add, name="add"),
+    path("add-group/", views.add_group, name='add_group'),
+    path("join-group/", views.join_group, name='join_group'),
+    path("my-group/", views.group_list, name='group_list'),
+  
+    path("about/", views.about, name="about"),
+    path("name=<name>", views.view, name="view"),
+
+    path("<path:file>", views.open_file, name='open_file'),
+    
+
+  
+]
+
+
+"""
+search page
+view whole page
+
+smiles/name/seq/id_serach = <id>
+
+"""
