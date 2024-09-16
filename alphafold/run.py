cf2e97853e56a739427aeb083ca21a153c04b479
MungShu <mungshushen@gmail.com>
2024-09-16 16:44:35 -0400

    alphafold

---
+++ alphafold/run.py
@@ -0,0 +1,23 @@
+import sys, os
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(prog = "run.py", add_help=True, 
+         description='export path to your localcolabfolder first using: export PATH="/path/to/your/localcolabfold/colabfold-conda/bin:$PATH"')
+
+parser.add_argument("--fa_files", required=True)
+
+args = vars(parser.parse_args())
+
+fa_files = args["fa_files"]
+
+for i in os.listdir(fa_files):
+    output_dir = i.split('.')[0]
+
+    print(f"-- Running AlphaFold on {output_dir}. Outputs in '{output_dir}/'. --")
+    subprocess.run(['colabfold_batch', '--templates', f'{fa_files}/{i}', output_dir])
+
+    print("-- AlphaFold completed. Transfering files. --")
+
+    subprocess.run(["expect", "transfer.exp", output_dir])
+    print(f"-- {output_dir} done --")
